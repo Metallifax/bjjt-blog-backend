4a6ef1b8f6cddb743f6a75080fe635b4c84337b5
Metallifax <mathew_forgione@msn.com>
2022-04-30 21:32:31 -0300

    fixed a bug where user wasn't being persisted in 'blog-backend' upon signup
    url was pointing to the root MongoDB url

--- src/config/default.js
+++ src/config/default.js
@@ -1,6 +1,6 @@
 export default {
   port: 4000,
-  dbUri: 'mongodb://user:user@localhost:27017/?authSource=admin',
+  dbUri: 'mongodb://user:user@localhost:27017/blog-backend?authSource=admin',
   tokenSecret: 'temp-secret',
   saltRounds: 10,
 };
