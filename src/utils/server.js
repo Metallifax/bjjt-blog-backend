c8e948f43cb28654f73e6e813de620967cd8aaeb
Metallifax <mathew_forgione@msn.com>
2022-05-06 12:01:56 -0300

    added cors to the project

--- src/utils/server.js
+++ src/utils/server.js
@@ -1,9 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 import authRoute from '../routes/auth.route.js';
 import userRoute from '../routes/user.route.js';
 
 const server = () => {
   const app = express();
+  app.use(cors());
   app.use(express.json());
   app.use('/api/auth', authRoute);
   app.use('/api/user', userRoute);
